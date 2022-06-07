7b03ad0af65834ff863ac2fe80b2352ca5a2b654
Trust <45748977+girage@users.noreply.github.com>
2022-06-07 22:14:16 +0700

    go5

---
+++ go5-convert-value/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	speed := 100 // speed is int
+	force := 2.5 //force is float64
+
+	// speed = speed * force // error becuz diff. type
+	// speed = speed * int(force) // => 200 (100 * 2)
+	// speed = float64(speed) * force // error becuz you speed is int
+	speed = int(float64(speed) * force) // 250
+
+	fmt.Println(speed)
+}
